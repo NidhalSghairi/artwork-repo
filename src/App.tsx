dc384407e9a8e02c64d5fe14fa3df985e738f762
NidhalSghairi <nidhal.sghairi@ensi-uma.tn>
2022-11-11 10:00:08 +0100

    feat(routing): add routing and fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { artworkId } = useParams();
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://storage.googleapis.com/ya-misc/interviews/front/examples/${artworkId}.json`
+      )
+      .then((response) => setData(response.data));
+  }, [artworkId]);
+
+  return <div></div>;
 }
 
 export default App;
